d6db0e69714a7290bf3f108baf8827185b3fbebf
Yael Harosh <yaelharosh01@gmail.com>
2023-04-02 10:35:11 -0400

    pass tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,10 @@
 
 def return_evens(num_list):
     pass
+    even = [n for n in num_list if n % 2 == 0]
+    return even
 
 def make_exclamation(sentence_list):
     pass
+    add = [n + '!' for n in sentence_list]
+    return add
